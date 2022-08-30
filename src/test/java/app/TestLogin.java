8315ff8b7225308d55e5af46c1b3d8ff90b2471c
DuongThiPhuongThao26 <thaodtp2610.dev@gmail.com>
2022-08-30 14:29:57 +0700

    Update

--- src/test/java/app/TestLogin.java
+++ src/test/java/app/TestLogin.java
@@ -3,13 +3,11 @@ package app;
 import Utils.Utils;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
-import org.openqa.selenium.WebDriver;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayName("JUNIT5")
 public class TestLogin extends BaseTest {
-    private static WebDriver driver;
 
     @Test
     public void testSignIn() {
@@ -18,5 +16,4 @@ public class TestLogin extends BaseTest {
         HomePage homePage = new HomePage(driver);
         assertEquals(Utils.urlExpected, homePage.getUrl());
     }
-
 }
