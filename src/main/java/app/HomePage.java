16752354cb38bf58a9a51e6b52eeb13b04081177
DuongThiPhuongThao26 <thaodtp2610.dev@gmail.com>
2022-10-06 14:43:20 +0700

    Update

--- src/main/java/app/HomePage.java
+++ src/main/java/app/HomePage.java
@@ -2,11 +2,15 @@ package app;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 public class HomePage extends BasePage {
     BasePage basePage;
 
     public static final By womenTab = By.xpath("//a[@id='ui-id-4']//span[contains(text(),'Women')]");
+    public static final By tops = By.xpath("//a[@id='ui-id-9']//span[contains(text(),'Tops')]");
+    public static final By tanks = By.xpath("//span[normalize-space()='Bras & Tanks']");
 
     public HomePage(WebDriver driver) {
         super(driver);
@@ -19,6 +23,12 @@ public class HomePage extends BasePage {
     }
 
     public void clickOnWomenTab() {
-        driver.findElement(womenTab).click();
+        WebElement women = driver.findElement(womenTab);
+        WebElement top = driver.findElement(tops);
+        WebElement tank = driver.findElement(tanks);
+
+        Actions actions = new Actions(this.driver);
+        actions.moveToElement(women).moveToElement(top).moveToElement(tank).click().build().perform();
+
     }
 }
